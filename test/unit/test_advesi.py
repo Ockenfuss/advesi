11671956862ee4864463e33935bd137707a89085
Paul Ockenfuss <42680748+Ockenfuss@users.noreply.github.com>
2023-10-12 14:07:35 +0200

    New test for trajectory calculation

--- test/unit/test_advesi.py
+++ test/unit/test_advesi.py
@@ -1,11 +1,9 @@
-#%%
 import unittest as ut
 import numpy as np
 import numpy.testing as npt
 import xarray as xr
 import xarray.testing as xrt
 import advesi as adv
-#%%
 
 
 class Test_Flowfield_Collection(ut.TestCase):
@@ -25,8 +23,8 @@ class Test_Flowfield_Collection(ut.TestCase):
     def test_flowfield_collection(self):
         u=xr.DataArray(np.arange(10), coords=[(('x', np.arange(10)))])
         ff=adv.FlowField_Collection(u,0.0, 0.0)
-        npt.assert_array_equal(ff.u.y, [-np.inf, np.inf]) #missing dimensions are added from -inf to inf
-        npt.assert_array_equal(ff.v.y, [-np.inf, np.inf]) #missing dimensions are added from -inf to inf
+        npt.assert_array_equal(ff.u.y, [-adv.FIELD_BOUNDARY, adv.FIELD_BOUNDARY]) #missing dimensions are added from -inf to inf
+        npt.assert_array_equal(ff.v.y, [-adv.FIELD_BOUNDARY, adv.FIELD_BOUNDARY]) #missing dimensions are added from -inf to inf
         npt.assert_array_equal(ff.v.values, np.zeros((2,2,2))) #floats are accepted and converted to infinitely valid arrays
 
         w=xr.DataArray(np.arange(10), coords=[(('w', np.arange(10)))]) #we can add additional dimensions to the flowfield components
@@ -34,7 +32,6 @@ class Test_Flowfield_Collection(ut.TestCase):
         assert({'x', 'y', 'z', 'w'}==set(ff.w.dims))
 
     def test_path_collection(self):
-        #%%
         x=xr.DataArray([[0,np.nan], [2,3]], coords=[('n', [0,1]), ('t', [0.1, 0.2])])
         y=xr.DataArray([[0,1], [np.nan,3]], coords=[('n', [0,1]), ('t', [0.1, 0.2])])
         z=x.copy()
@@ -42,4 +39,14 @@ class Test_Flowfield_Collection(ut.TestCase):
         expected=xr.DataArray([[0,np.nan], [np.nan,3]], coords=[('n', [0,1]), ('t', [0.1, 0.2])]) #nan in one coordinate leads to nans in other coordinates as well
         xrt.assert_equal(path_coll.x, expected)
         xrt.assert_equal(path_coll.y, expected)
+
+class Test_TrajectoryCollection(ut.TestCase):
+    def test_backward(self):
+        #steady 3 m/s flow
+        u=xr.DataArray([3.0, 3.0], coords=[('z', [0,1.0])]).astype(float)
+        ff=adv.FlowField_Collection(u, 0,0)
+        traj_coll=adv.Trajectory_Collection.from_flowfield(ff, np.array([0.0]), np.array([0.0]), np.array([0.5]), 0.1, 10, steps_backward=10)
+        #after 0.2s, the particle should be at 0.6m
+        self.assertAlmostEqual(traj_coll.x.sel(T=0.2, method='nearest').item(), 0.6)
+        self.assertAlmostEqual(traj_coll.x.sel(T=-0.2, method='nearest').item(), -0.6)
 
